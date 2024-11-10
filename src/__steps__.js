d4285913738bc8e7404c729659a62310ebc117a0
Ostitter-Anondo <89913816+Ostitter-Anondo@users.noreply.github.com>
2024-11-10 17:09:45 +0600

    some new stuff

--- src/__steps__.js
+++ src/__steps__.js
@@ -4,6 +4,7 @@
 ~ get firebase config P.S. never save config client side
 ~ create auth in config & export auth directly
 ~ enable google provider in build > authentication > sign-in method
-~ 
+~ implement auth functions and use providers
+~ login! you're golden
 ~ 
 */
